57ace09d03f4d8ea827141f2cd74b1a2edf388c4
Harish Krupo <harishkrupo@gmail.com>
2020-10-25 14:17:25 -0400

    WIP strategy
    Signed-off-by: Harish Krupo <harishkrupo@gmail.com>

--- bots/strategy.py
+++ bots/strategy.py
@@ -1,8 +1,10 @@
 from bots.chase import Chase
+from bots.shoot import Shoot
+from bots.sequence_and_selector import Sequence, Selector
 
 class Strategy():
     def __init__(self):
-        self.spec = [Chase]
+        self.spec = [(Sequence, "sequence"), (Chase, "Chaser"), (Shoot, "Shooter")]
 
     def getStrategy(self):
         return self.spec
