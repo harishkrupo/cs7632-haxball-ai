b5ef9867a4e4b518a8b305dc6d609ac0d6acc49e
Harish Krupo <harishkrupo@gmail.com>
2020-10-25 14:17:01 -0400

    [cs7632] Add action_chase.py
    Signed-off-by: Harish Krupo <harishkrupo@gmail.com>

--- bots/action_chase.py
+++ bots/action_chase.py
@@ -1,25 +1,35 @@
 from bots.btnode import BTNode
 import replay
+import numpy as np
 
 class Chase(BTNode):
     def execute(self):
         BTNode.execute(self)
         gameworld = self.agent
         if gameworld.player:
-            t = 0
+            t = 36
             px = gameworld.player.disc.x
             py = gameworld.player.disc.y
 
             bx = gameworld.game.ball.x
             by = gameworld.game.ball.y
 
+            xdiff = abs(px - bx)
+            ydiff = abs(py - by)
+            dist = np.sqrt(xdiff ** 2 + ydiff ** 2)
+
             inputs = []
-            if abs(px - bx) > t:
+            if dist < t:
+                return True
+
+            xproximity = xdiff > 0
+            yproximity = ydiff > 0
+            if xproximity:
                 inputs.append(replay.Input.Right if px < bx else replay.Input.Left)
 
-            if abs(py - by) > t:
+            if yproximity:
                 inputs.append(replay.Input.Down if py < by else replay.Input.Up)
 
             gameworld.setInput(*inputs)
             return None
-        return True
+        return False
