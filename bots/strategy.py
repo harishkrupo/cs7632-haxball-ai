c2bff218b7ed7579ae117b754cbac3448cffd811
Harish Krupo <harishkrupo@gmail.com>
2020-10-25 14:17:25 -0400

    [cs7632] Add action_strategy.py
    Signed-off-by: Harish Krupo <harishkrupo@gmail.com>

--- bots/strategy.py
+++ bots/strategy.py
@@ -1,8 +1,14 @@
-from bots.chase import Chase
+from bots.action_chase import Chase
+from bots.action_shoot import Shoot
+from bots.sequence import Sequence
+from bots.selector import Selector
+from bots.action_align import Align
 
 class Strategy():
     def __init__(self):
-        self.spec = [Chase]
+        self.spec = [(Sequence, "sequence"), (Chase, "Chaser"), (Align, "Aligner"), (Shoot, "Shooter")]
+        # self.spec = [(Sequence, "sequence"), (Chase, "Chaser"), (Align, "Aligner")]
+        # self.spec = [(Chase, "Chaser")]
 
     def getStrategy(self):
         return self.spec
