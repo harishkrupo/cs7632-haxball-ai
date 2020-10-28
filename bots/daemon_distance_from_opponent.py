588b23c99efdf1bc397856a1e1a9fbd143f897cb
Divyanshu Goyal <divyanshu.g25@gmail.com>
2020-10-28 19:03:32 -0400

    [cs-7632] Added Daemon Distance_From_Ball
    Signed-off-by: Divyanshu Goyal <divyanshu.g25@gmail.com>
    Signed-off-by: Harish Krupo <harishkrupo@gmail.com>

---
+++ bots/daemon_distance_from_opponent.py
@@ -0,0 +1,40 @@
+from bots.btnode import BTNode
+from replay import *
+import numpy as np
+
+
+class ClosestTeamToBall(BTNode):
+    def parseArgs(self, args):
+        if len(args) > 0:
+            self.id = args[0]
+            # 0:Red and 1:Blue Look in replay.py
+            self.team = args[1]
+            self.dist = args[2]
+
+    def execute(self):
+        BTNode.execute(self)
+        gameworld = self.agent
+        if gameworld.player:
+            game = gameworld.game
+            player = [
+                game_player
+                for game_player in game.players
+                if game_player.team == self.team
+            ][0]
+            opponent_player = [
+                game_player
+                for game_player in game.players
+                if not game_player.team == self.team
+            ][0]
+
+            # TODO: Modify it for 2v2 or 3v3 case.
+
+            # ball_loc = np.array([game.ball.x, game.ball.y])
+            player_loc = np.array([player.disc.x, player.disc.y])
+            opponent_player_loc= np.array([opponent_player.disc.x, opponent_player.disc.y])
+            # player_dist = np.linalg.norm(player_loc - ball_loc)
+            opponent_player_dist = np.linalg.norm(opponent_player_loc - player_loc)
+            if opponent_player_dist < self.dist:
+                return True
+            return False
+        return None
