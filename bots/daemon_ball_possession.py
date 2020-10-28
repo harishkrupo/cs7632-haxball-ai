07ea7b211b008dfd8d5dcad669111657fc5469ec
Divyanshu Goyal <divyanshu.g25@gmail.com>
2020-10-28 19:00:49 -0400

    [cs-7632] Added Ball_Possession_Daemon
    Signed-off-by: Divyanshu Goyal <divyanshu.g25@gmail.com>
    Signed-off-by: Harish Krupo <harishkrupo@gmail.com>

---
+++ bots/daemon_ball_possession.py
@@ -0,0 +1,48 @@
+from bots.btnode import BTNode
+from replay import *
+import numpy as np
+
+
+class BallPossession(BTNode):
+    def parseArgs(self, args):
+        if len(args) > 0:
+            self.id = args[0]
+            # 0:Red and 1:Blue Look in replay.py
+            self.team = args[1]
+            self.is_mine = args[2]
+
+    def execute(self):
+        BTNode.execute(self)
+        gameworld = self.agent
+        if gameworld.player:
+            t = 36
+            player = gameworld.player
+            game = gameworld.game
+            pteam = player.team
+            bx = game.ball.x
+            by = game.ball.y
+
+            if self.is_mine:
+                px = player.disc.x
+                py = player.disc.y
+                xdiff = abs(px - bx)
+                ydiff = abs(py - by)
+                dist = np.sqrt(xdiff ** 2 + ydiff ** 2)
+                if pteam == self.team and dist < t:
+                    return True
+            else:
+                enemy_player = [
+                    game_player
+                    for game_player in game.players
+                    if not game_player.team == player.team
+                ][0]
+                px = enemy_player.disc.x
+                py = enemy_player.disc.y
+                xdiff = abs(px - bx)
+                ydiff = abs(py - by)
+                dist = np.sqrt(xdiff ** 2 + ydiff ** 2)
+                if pteam != self.team and dist < t:
+                    return True
+
+            return False
+        return None
