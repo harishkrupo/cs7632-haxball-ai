558b4d899ec027b8f6417c1c86fad50bea4c6032
kingsman142 <kingsman142@yahoo.com>
2020-10-31 19:44:39 -0400

    Added sample strategy specs for the james nodes

--- bots/strategy.py
+++ bots/strategy.py
@@ -3,12 +3,18 @@ from bots.action_shoot import Shoot
 from bots.sequence import Sequence
 from bots.selector import Selector
 from bots.action_align import Align
+from bots.daemon_clearpathtogoal import ClearPathToGoalDaemon
+from bots.daemon_sideofthefield import SideOfTheFieldDaemon
+from bots.action_movetoopponentside import MoveToOpponentSideAction
+from bots.action_movebetweenopponentandgoal import MoveBetweenOpponentAndGoal
+
 
 class Strategy():
     def __init__(self):
         self.spec = [(Sequence, "sequence"), (Chase, "Chaser"), (Align, "Aligner"), (Shoot, "Shooter")]
         # self.spec = [(Sequence, "sequence"), (Chase, "Chaser"), (Align, "Aligner")]
         # self.spec = [(Chase, "Chaser")]
+        # self.spec = [MoveToOpponentSideAction] [(MoveBetweenOpponentAndGoal, 0.75)] [MoveToOpponentSideAction] [SideOfTheFieldDaemon] [ClearPathToGoalDaemon] -- EXAMPLE ONLY
 
     def getStrategy(self):
         return self.spec
