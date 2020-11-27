cedbebef75e2b21f4fa53e88b3577aad148be927
Harish Krupo <harishkrupo@gmail.com>
2020-11-27 14:20:27 -0500

    Add difficulty to align
    Signed-off-by: Harish Krupo <harishkrupo@gmail.com>

--- bots/action_align.py
+++ bots/action_align.py
@@ -20,6 +20,10 @@ class Align(BTNode):
                 goal_top = locations["btg"]
                 goal_bottom = locations["bbg"]
 
+            goal_half = (goal_top[1] - goal_bottom[1])/2
+            goal_top[1] = goal_top[1] - goal_half * (1 - gameworld.get_difficulty())
+            goal_bottom[1] = goal_bottom[1] + goal_half * (1 - gameworld.get_difficulty())
+
             ball_top_goal_vector = [goal_top[0] - bx, goal_top[1] - by]
             ball_bottom_goal_vector = [goal_bottom[0] - bx, goal_bottom[1] - by]
             player_ball_vector = [px - bx, py - by]
@@ -34,7 +38,6 @@ class Align(BTNode):
 
             inputs = []
 
-            # print(playerball_cross_topgoalball, playerball_cross_bottomgoalball)
             ret = True
             if gameworld.player.team == Team.Blue:
                 if not (goal_top[0] < bx < px):
